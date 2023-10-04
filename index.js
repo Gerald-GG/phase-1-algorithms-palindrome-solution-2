d48c0833c3f803c47725b845f9fce2cab823798f
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-05 01:24:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(i = 0; i<word.length / 2;i++){
+    const j = word.length-1-i;
+
+    if (word[i] !== word[j]) return false
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
+  This code compares characters of index "i" with those of index "j" and it returns 
+  false if the characters do not match.
+  When the loop completes without a mismatch, it returns true.
 */
 
 // You can run `node index.js` to view these console logs
